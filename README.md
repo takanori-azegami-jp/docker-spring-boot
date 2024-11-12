68b67f290d0277afd614644d9321dc3be9e93e1f
takanori.azegami <takanori.azegami.ta@ultmarc.jp>
2024-11-12 21:07:04 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # docker-spring-boot
-Docker 環境で Spring Boot Webサービスを起動する
+
+Docker 環境で Spring Boot Web サービスを起動する
+
+## 参考
+
+- [Docker 環境で Spring Boot Web サービスを起動する](https://qiita.com/studio_meowtoon/items/9c07e20b4124d8c5f972)
+- [Spring Boot Web サービスで Hello World する](https://qiita.com/studio_meowtoon/items/37ac0082a3228e4de570)
---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@RequestMapping("/api")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/data")
+    public Map<String, String> getData() {
+        Map<String, String> map = Map.of("message", "Hello World!");
+        return map;
+    }
+}
